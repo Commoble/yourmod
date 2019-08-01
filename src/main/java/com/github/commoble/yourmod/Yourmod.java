363563e56c7aa7709bfa70922139756366a17c22
Commoble <josephbettendorff@yahoo.com>
2019-08-01 12:44:38 -0500

    Improve build.gradle

--- src/main/java/com/github/commoble/yourmod/Yourmod.java
+++ src/main/java/com/github/commoble/yourmod/Yourmod.java
@@ -2,8 +2,8 @@ package com.github.commoble.yourmod;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(Yourmod.MODID)
-public class Yourmod
+@Mod(YourMod.MODID)
+public class YourMod
 {
 	public static final String MODID = "yourmod";
 }
